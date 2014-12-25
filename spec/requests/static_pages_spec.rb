5fb776ca6ca7e6ed6bf23fc23814a8cf4793eac8
Mariko <maripiyok@gmail.com>
2014-12-25 14:39:01 +0900

    タイトルのテストを追加（レイアウト利用）

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe "StaticPages", :type => :request do
       visit static_pages_home_path
       expect(page).to have_content('Recipes Index App')
     end
+
+    it "should have the right title" do
+      visit static_pages_home_path
+      expect(page).to have_title('Recipes Index App | Home')
+    end
   end
 
   describe "Help page" do
@@ -18,12 +23,22 @@ RSpec.describe "StaticPages", :type => :request do
       visit static_pages_help_path
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit static_pages_help_path
+      expect(page).to have_title('Recipes Index App | Help')
+    end
   end
 
   describe "About page" do
     it "shuld have the content About Us" do
       visit static_pages_about_path
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit static_pages_about_path
+      expect(page).to have_title('Recipes Index App | About Us')
     end
   end
 end
