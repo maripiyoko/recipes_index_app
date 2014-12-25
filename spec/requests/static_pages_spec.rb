31a6db468cda470976303760fa2ce0900817f20b
Mariko <maripiyok@gmail.com>
2014-12-25 14:16:32 +0900

    StaticPages に about usを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe "StaticPages", :type => :request do
       expect(page).to have_content('Recipes Index App')
     end
   end
+
+  describe "Help page" do
+    it "should have the content Help" do
+      visit static_pages_help_path
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "shuld have the content About Us" do
+      visit static_pages_about_path
+      expect(page).to have_content('About Us')
+    end
+  end
 end
