dfc6196ab1a02deb3df0e9a7bff784edfaefda07
Mariko <maripiyok@gmail.com>
2014-12-25 13:52:19 +0900

    RSpec設定変更
    rails generate integration_test コントローラー名
    でrequest_specが作成されるようにした。

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module RecipesIndexApp
         helper_specs: false,
         routing_specs: false,
         controller_specs: true,
-        request_specs: false
+        request_specs: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
   end
