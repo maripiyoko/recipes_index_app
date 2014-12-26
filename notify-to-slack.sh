303a71ab7a782fc1dbb854d5df15ae26bf3be67f
Mariko <maripiyok@gmail.com>
2014-12-26 11:55:35 +0900

    Change slack notice to shell

---
+++ notify-to-slack.sh
@@ -0,0 +1,2 @@
+curl -X POST --data-urlencode 'payload={"channel": "#development", "username": "Circle CI", "text": "production環境へのデプロイ完了しました。"}' https://hooks.slack.com/services/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
+
