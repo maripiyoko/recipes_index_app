9c8661130bbb75f5524f3119c5c1a5d676207795
Mariko <maripiyok@gmail.com>
2014-12-26 12:00:23 +0900

    Remove notify shell. try setting webhooks to hubot

--- notify-to-slack.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST --data-urlencode 'payload={"channel": "#development", "username": "Circle CI", "text": "production環境へのデプロイ完了しました。"}' https://hooks.slack.com/services/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
-
