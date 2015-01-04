7240d74e6bff9bbc6500e581557f3d0b288786a3
Mariko <maripiyok@gmail.com>
2015-01-05 08:09:35 +0900

    Add spec test for password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, :type => :model do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
 
   it { should be_valid }
 
