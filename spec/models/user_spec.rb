847154a45fba2e3d2c20b622adfb410a0ef79a66
Mariko <maripiyok@gmail.com>
2015-01-06 08:03:01 +0900

    Rspec 3 use be_falsey instead of be_false.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe User, :type => :model do
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
       it { should_not eq user_for_invalid_password }
-      specify { expect(user_for_invalid_password).to be_false }
+      specify { expect(user_for_invalid_password).to be_falsey }
     end
   end
 
