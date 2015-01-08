122f8e4af6812a2cfd1af1f94d189e999efccbde
Mariko <maripiyok@gmail.com>
2015-01-09 07:52:18 +0900

    Add user's RESTful routes. Remove users/new specific route. Add user's show controller and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  resources :users
 
   root 'static_pages#home'
 
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
