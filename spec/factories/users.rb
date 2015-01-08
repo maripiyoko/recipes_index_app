799e92d0c3840a4033e3b5e634bd97d4807ce2f4
Mariko <maripiyok@gmail.com>
2015-01-09 08:17:29 +0900

    Add user's profile page spec. Add users factory. Modify show view to pass the spec.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-email "MyString"
+    name "Example User"
+    sequence(:email) { |n| "example_user#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
   end
 
 end
