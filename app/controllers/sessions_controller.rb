c06196fa9ac6a9e17a9d3601b0e3b4cf6723b926
Mariko <maripiyok@gmail.com>
2015-01-28 08:25:00 +0900

    Add sign in error handler and its spec.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # success sign in
+    else
+      # error show message and redirect to signin page
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
