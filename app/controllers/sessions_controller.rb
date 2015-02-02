6ed28dc046b9d25ec4db86dab73846c99aad8b08
Mariko <maripiyok@gmail.com>
2015-02-03 08:10:09 +0900

    Add sign_in session helper function.
    Add config setting that SessionsHelper can be used via controller too.
    Add controller method to sign_in user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # success sign in
+      sign_in user
+      redirect_to user
     else
       # error show message and redirect to signin page
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,31 @@
 module SessionsHelper
+
+  ## ユーザーをサインインさせるヘルパ関数
+  ## ページ移動ではパスワードやパスワード確認が取得できないため
+  ## ページ移動時はここで保存したremember_tokenを元に
+  ## データベースを検索してユーザーを取得する
+  def sign_in(user)
+    # トークンの新規作成
+    remember_token = User.new_remember_token
+    # 暗号化されていないトークンをブラウザクッキーに保存
+    cookies.permanent[:remember_token] = remember_token
+    # 暗号化したトークンをデータベースに保存
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    # 与えられたユーザーを現在のユーザーにする
+    # current_userはコントローラー、ビュー両方からアクセス可能
+    self.current_user = user
+  end
+
+
+  # current_userへの要素代入
+  def current_user=(user)
+    @current_user = user
+  end
+
+  # current_userを取得
+  # remember_tokenを暗号化したものでデータベースを検索する
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
 end
