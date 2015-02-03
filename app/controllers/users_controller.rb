cad0c9f010d97e3e9b795c056d2458c49d368146
Mariko <maripiyok@gmail.com>
2015-02-04 08:02:30 +0900

    Change immediately sign in to app after creating new user. Add spec for it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # success
+      sign_in @user
       flash[:success] = "Welcome to the Recipes Index App!"
       redirect_to @user
     else
